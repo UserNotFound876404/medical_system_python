121bbb0c29e5c583bf19d3d58d04f870cdc21f0f
UserNotFound876404 <usernotfound876594@gmail.com>
2025-12-04 19:30:05 +0800

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ PORT: Final[str] = os.getenv('PORT')
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return "home", 200
 
@@ -58,4 +58,5 @@ def health():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
+
 
